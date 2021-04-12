fe5bb117675865e363c1a4b2ef4d4d4ff4ecb30e
Boukharta <anasboukharta@gmail.com>
2021-04-12 12:06:51 +0000

    adding component in react client, wip 3

--- client/src/Components/PostUser.tsx
+++ client/src/Components/PostUser.tsx
@@ -46,36 +46,45 @@ const PostUser = (props: postUser) => {
         }
     }
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
         console.log(values.name, values.email, values.password)
-      const target = event.target;
-      const val = target.value
-      setValues({
-        ...values,
-        name: val,
-      })
+     if (values.name ) {
+            postUser()
+     } else {
+         alert('Invalid user details')
+     }
     }
+ 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+         const target = event.target
+         const val = target.value
+         setValues({
+             ...values,
+             name: val,
+         })
+     }
+
 
     return (
-        <form className="Form" onSubmit={(e) => handleSaveUser(e, formData)}>
+        <form className="Form" onSubmit={(e) => handleSubmit(e)}>
             <div>
                 <div>
                     <label htmlFor="name">Title</label>
-                    <input onChange={handleForm} type="text" id="title" />
+                    <input onChange={handleChange} type="text" id="title" />
                 </div>
                 <div>
                     <label htmlFor="email">email</label>
-                    <input onChange={handleForm} type="text" id="email" />
+                    <input onChange={handleChange} type="text" id="email" />
                   </div>
                 <div>
                     <label htmlFor="password">password</label>
-                    <input onChange={handleForm} type="text" id="password" />
+                    <input onChange={handleChange} type="text" id="password" />
                 </div>
             </div>
-            <button>Create User</button>
+            <button type="submit">Create User</button>
         </form>
     )
 }
 
-export default CreateUser
+export default PostUser
